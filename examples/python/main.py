5268f44870a13923355c3fa1fcd341bef5a34aa5
Øystein Bringsli <oysteinmb@gmail.com>
2026-04-20 14:32:07 +0200

    Better visu

--- examples/python/main.py
+++ examples/python/main.py
@@ -4,125 +4,111 @@ from time import perf_counter
 from SerialCommPython import SerialComm, SensorData, Command
 import pg_widgets as pw
 
-def main():
-    serialComm = SerialComm("COM3", 460800)
-    controlManager = pw.ControlManager()
-
+def getPlotPos():
     plotPos = pw.Plot((0.0, 0.0), (0.5, 0.33))
     plotPos.setTitle("Position")
     plotPos.setXLabel("Time (s)")
     plotPos.setYLabel("Position (rad)")
-    controlManager["plotPos"] = plotPos
+    return plotPos
 
+def getPlotVel():
     plotVel = pw.Plot((0.0, 0.33), (0.5, 0.33))
     plotVel.setTitle("Velocity")
     plotVel.setXLabel("Time (s)")
     plotVel.setYLabel("Velocity (rad/s)")
-    controlManager["plotVel"] = plotVel
+    return plotVel
 
+def getPlotCurrent():
     plotCurrent = pw.Plot((0.0, 0.66), (0.5, 0.33))
     plotCurrent.setTitle("Current")
     plotCurrent.setXLabel("Time (s)")
     plotCurrent.setYLabel("Current (mA)")
-    controlManager["plotCurrent"] = plotCurrent
+    return plotCurrent
 
-    controlManager["sensorText"] = pw.TextBoxes((0.5, 0.0), (0.25, 0.5), labels=["Position", "Velocity", "Torque", "Current"])
-    groupToggle = pw.UIGroup((0.75, 0.0), (0.25, 0.5))
-    groupToggle["OnOffText"] = pw.TextBox((0.0, 0.0), (1.0, 0.25))
-    groupToggle["OnOffText"].setText("Off / On")
-    groupToggle["posText"] = pw.TextBox((0.0, 0.25), (0.5, 0.25))
-    groupToggle["posText"].setText("Position")
-    groupToggle["velText"] = pw.TextBox((0.0, 0.5), (0.5, 0.25))
-    groupToggle["velText"].setText("Velocity")
-    groupToggle["torText"] = pw.TextBox((0.0, 0.75), (0.5, 0.25))
-    groupToggle["torText"].setText("Torque")
-    groupToggle["posToggle"] = pw.ToggleButton((0.5, 0.25), (0.5, 0.25))
-    groupToggle["velToggle"] = pw.ToggleButton((0.5, 0.5), (0.5, 0.25))
-    groupToggle["torToggle"] = pw.ToggleButton((0.5, 0.75), (0.5, 0.25))
+def getTextBoxesDriverData():
+    labelsDescription = [
+        "Iteration",
+        "Timestamps (ms): ",
+        "Position: ", "Velocity: ", "Torque: ", "Current: ",
+        "Loop Motor (us): ",
+        "Loop Comm (us): "
+    ]
+    labelsData = ["0"] * len(labelsDescription)
+    textBoxesDescription = pw.TextBoxes((0.5, 0.0), (0.25, 0.5), labels=labelsDescription)
+    textBoxesData = pw.TextBoxes((0.75, 0.0), (0.25, 0.5), labels=labelsData)
+    return textBoxesDescription, textBoxesData
 
-    controlManager["configGroup"] = groupToggle
-
+def getTuningSliders():
     labels = ["Setpoint", "Kp", "Ki", "Kd"]
     upper_bounds = [-5.0, 0.0, 0.0, 0.0]
     lower_bounds = [5.0, 0.5, 0.0000015, 0.0000001]
     current_vals = [0.0, 0.005, 0.000000, 0.0]
-    controlManager["tuningSlider"] = pw.TuningSliders((0.5, 0.5), (0.5, 0.5),
+    slider = pw.TuningSliders((0.5, 0.5), (0.5, 0.45),
         labels=labels, lower_bounds=lower_bounds, upper_bounds=upper_bounds, current_values=current_vals)
+    return slider
 
-    sliderIx = 0
-    cmd = Command()
-    cmd.command_type = 12
-    cmd.value0 = 3
-    serialComm.send_data(cmd)
+def generateTab(name):
+    # Making a tab for tuning, having 3x plots on the left
+    # Feedback text and tuning sliders on the right.
+
+    gr = pw.UIGroup((0.0, 0.0), (1.0, 1.0))
+    gr["plotPos"] = getPlotPos()
+    gr["plotVel"] = getPlotVel()
+    gr["plotCurrent"] = getPlotCurrent()
+
+    descriptionBoxes, dataBoxes = getTextBoxesDriverData()
+    gr["textBoxesDescription"] = descriptionBoxes
+    gr["textBoxesData"] = dataBoxes
+    gr["tuningSliders"] = getTuningSliders()
+
+    gr["nameText"] = pw.TextBox((0.5, 0.95), (0.3, 0.05))
+    gr["nameText"].setText(name)
+    return gr
+
+def generateMainTab(name):
+    gr = pw.UIGroup((0.0, 0.0), (1.0, 1.0))
+    gr["plotPos"] = getPlotPos()
+    gr["plotVel"] = getPlotVel()
+    gr["plotCurrent"] = getPlotCurrent()
+
+    descriptionBoxes, dataBoxes = getTextBoxesDriverData()
+    gr["textBoxesDescription"] = descriptionBoxes
+    gr["textBoxesData"] = dataBoxes
+
+    gr["textOnOff"] = pw.TextBox((0.5, 0.5), (0.5, 0.1))
+    gr["textOnOff"].setText("Driving Mode (on / off)")
+
+    gr["descriptions"] = pw.TextBoxes((0.5, 0.6), (0.25, 0.35), labels = ["Position", "Velocity", "Torque"])
+    gr["togglePos"] = pw.ToggleButton((0.75, 0.6), (0.25, 0.1166666))
+    gr["toggleVel"] = pw.ToggleButton((0.75, 0.7166666), (0.25, 0.1166666))
+    gr["toggleTor"] = pw.ToggleButton((0.75, 0.8333333), (0.25, 0.1166666))
+
+    gr["nameText"] = pw.TextBox((0.5, 0.95), (0.3, 0.05))
+    gr["nameText"].setText(name)
+
+    return gr
+
+def main():
+    enableSerial = False
+
+    if enableSerial: serialComm = SerialComm("COM4", 460800)
+    controlManager = pw.ControlManager()
+
+    mainTab = generateMainTab("Main")
+    posTab = generateTab("Position")
+    velTab = generateTab("Velocity")
+    torTab = generateTab("Torque")
+    tabs = pw.Tab((0.0, 0.0), (1.0, 1.0), [mainTab, posTab, velTab, torTab])
+    controlManager["tabs"] = tabs
+
+    prevValues = {}
 
     while controlManager.isRunning():
-        if (serialComm.has_sent_data()):
-            configType = 0
-            if (controlManager["configGroup"]["posToggle"].getValue()):
-                # PositionConfig
-                configType = 1
-            elif (controlManager["configGroup"]["velToggle"].getValue()):
-                # VelocityConfig
-                configType = 2
-            elif (controlManager["configGroup"]["torToggle"].getValue()):
-                # TorqueConfig
-                configType = 3
 
-            valueType = 0
-            if (sliderIx == 0):
-                # Setpoint
-                valueType = 1
-            elif (sliderIx == 1):
-                # Kp
-                valueType = 2
-            elif (sliderIx == 2):
-                # Ki
-                valueType = 3
-            elif (sliderIx == 3):
-                # Kd
-                valueType = 4
-
-            values = controlManager["tuningSlider"].getValue()
-            v = values[sliderIx]
-
-            if (valueType == 0) or (configType == 0):
-                cmd.command_type = 0
-            elif (valueType == 1):
-                # Setpoint
-                cmd.command_type = 9
-                if (configType == 1): cmd.command_type += 2
-                elif (configType == 2): cmd.command_type += 1
-                elif (configType == 3): cmd.command_type += 0
-            elif (configType == 1):
-                cmd.command_type = 4 + valueType
-            elif (configType == 2):
-                cmd.command_type = 1 + valueType
-            elif (configType == 3) and (valueType != 4):
-                cmd.command_type = -1 + valueType
-
-            print(f"Config Type: {configType} | Value type: {valueType} | CMD: {cmd.command_type} | Value: {v}")
-            cmd.value1 = v
-            serialComm.send_data(cmd)
-
-            sliderIx += 1
-            if (sliderIx == len(values)):
-                sliderIx = 0
-
-        didGetData, prevData = serialComm.get_data()
-
-        if (didGetData):
-            data: SensorData
-            texts = []
-            texts.append(f"Position: {prevData.position:.3f}")
-            texts.append(f"Velocity: {prevData.velocity:.3f}")
-            texts.append(f"Torque: {prevData.torque}")
-            texts.append(f"Current: {prevData.current:.3f}")
-            controlManager["sensorText"].setTexts(texts)
-
-            currTime = perf_counter()
-            controlManager["plotPos"].addValue(currTime, prevData.position, maxLength=1000)
-            controlManager["plotVel"].addValue(currTime, prevData.velocity, maxLength=1000)
-            controlManager["plotCurrent"].addValue(currTime, prevData.current, maxLength=1000)
+        if enableSerial:
+            didGetData, prevData = serialComm.get_data()
+            if (didGetData):
+                data: SensorData
 
         renderTime = controlManager.getRenderTime()
         # print(f"Rendertime: {renderTime}")
