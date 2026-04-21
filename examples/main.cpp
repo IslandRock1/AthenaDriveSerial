118edccf8dc84c40cd747ad30a84bb1cefe3c0d5
Øystein Bringsli <oysteinmb@gmail.com>
2026-04-21 11:16:38 +0200

    Simple test program

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <chrono>
+#include <cmath>
 #include "SerialComm/SerialComm.hpp"
 
 int main() {
-    SerialComm serialComm("COM3");
-
+    SerialComm serialComm("/dev/ttyACM0");
     std::cout << "Connected. Receiving data from ESP32...\n";
-    std::cout << "(Press 's' + Enter to send a command)\n";
 
-    Command cmd{3, 0, 1.0};
+    Command cmd{CommandType::TorqueSetpoint, 0, 0.0};
     serialComm.sendData(cmd);
 
-    cmd.commandType = 1;
-    int commandIteration = 1;
+    cmd.command_type = CommandType::DrivingModeCommand;
+    cmd.value0 = DrivingMode::Torque;
+    serialComm.sendData(cmd);
 
-    while (true) {
-        if (commandIteration++ % 100 == 0) {
+    cmd.command_type = CommandType::TorqueSetpoint;
+
+    auto t0 = std::chrono::high_resolution_clock::now();
+    auto prevSendTime = t0;
+    while (1) {
+        auto timeNow = std::chrono::high_resolution_clock::now();
+        auto timeSinceSend = std::chrono::duration_cast<std::chrono::milliseconds>(timeNow - prevSendTime).count();
+        if (timeSinceSend > 50) {
+            prevSendTime = timeNow;
+            auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(timeNow - t0).count();
+            auto t = static_cast<double>(ms) / 1000.0;
+
+            auto torque = std::sin(t) / 5.0;
+            cmd.value1 = torque;
             serialComm.sendData(cmd);
         }
 
-        // Read and cache the latest packet from the ESP32
-        if (serialComm.update()) {
-            SensorData data{};
-            serialComm.getData(data);
-            std::cout << "Received: iter=" << data.iteration
-                      << ", time=" << data.timestampMS
-                      << " ms, pos=" << data.position
-                      << ", vel=" << data.velocity << "\n";
-        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
