7a78c90618fd6d7152634bd89f48e7b9f502cb35
IslandRock1 <oysteinmb@gmail.com>
2026-05-08 22:45:07 +0200

    Add openloop

--- examples/python/main.py
+++ examples/python/main.py
@@ -47,15 +47,20 @@ def getTuningSliders(name):
     if (name == "Position"):
         upper_bounds = [0.0, 0.0, 0.0, 0.0]
         lower_bounds = [3.1415926 / 2.0, 5.0, 0.0, 0.0]
-        current_vals = [0.0, 0.000, 0.000000, 0.0]
+        current_vals = [0.0, 0.0, 0.0, 0.0]
     elif (name == "Velocity"):
         upper_bounds = [-150.0, 0.0, 0.0, 0.0]
         lower_bounds = [150.0, 0.1, 0.000001, 0.0]
-        current_vals = [0.0, 0.03, 0.000000, 0.0]
+        current_vals = [0.0, 0.0, 0.0, 0.0]
     elif (name == "Torque"):
         upper_bounds = [-1.0, 0.0, 0.0, 0.0]
         lower_bounds = [1.0, 0.005, 0.0, 0.0]
-        current_vals = [0.0, 0.000, 0.000000, 0.0]
+        current_vals = [0.0, 0.0, 0.0, 0.0]
+    elif (name == "OpenLoop"):
+        labels = ["Speed", "Strength"]
+        upper_bounds = [1.0, 1.0]
+        lower_bounds = [-1.0, 0.0]
+        current_vals = [0.0, 0.0]
 
     slider = pw.TuningSliders((0.5, 0.5), (0.5, 0.45),
         labels=labels, lower_bounds=lower_bounds, upper_bounds=upper_bounds, current_values=current_vals)
@@ -92,10 +97,11 @@ def generateMainTab(name):
     gr["textOnOff"] = pw.TextBox((0.5, 0.5), (0.5, 0.1))
     gr["textOnOff"].setText("Driving Mode (off / on)")
 
-    gr["descriptions"] = pw.TextBoxes((0.5, 0.6), (0.25, 0.35), labels = ["Position", "Velocity", "Torque"])
-    gr["togglePos"] = pw.ToggleButton((0.75, 0.6), (0.25, 0.1166666))
-    gr["toggleVel"] = pw.ToggleButton((0.75, 0.7166666), (0.25, 0.1166666))
-    gr["toggleTor"] = pw.ToggleButton((0.75, 0.8333333), (0.25, 0.1166666))
+    gr["descriptions"] = pw.TextBoxes((0.5, 0.6), (0.25, 0.35), labels = ["Position", "Velocity", "Torque", "OpenLoop"])
+    gr["togglePos"] = pw.ToggleButton((0.75, 0.6 + 0.0875 * 0), (0.25, 0.0875))
+    gr["toggleVel"] = pw.ToggleButton((0.75, 0.6 + 0.0875 * 1), (0.25, 0.0875))
+    gr["toggleTor"] = pw.ToggleButton((0.75, 0.6 + 0.0875 * 2), (0.25, 0.0875))
+    gr["toggleOpe"] = pw.ToggleButton((0.75, 0.6 + 0.0875 * 3), (0.25, 0.0875))
 
     gr["nameText"] = pw.TextBox((0.5, 0.95), (0.3, 0.05))
     gr["nameText"].setText(name)
@@ -103,7 +109,7 @@ def generateMainTab(name):
     return gr
 
 def main():
-    enableSerial = True
+    enableSerial = False
 
     if enableSerial: serialComm = SerialComm("COM4", 460800)
     if enableSerial: serialComm.send_data(Command(CommandType.SetCurrentLimit_mA, 10000))
@@ -113,7 +119,8 @@ def main():
     posTab = generateTab("Position")
     velTab = generateTab("Velocity")
     torTab = generateTab("Torque")
-    tabs = pw.Tab((0.0, 0.0), (1.0, 1.0), [mainTab, posTab, velTab, torTab])
+    opeTab = generateTab("OpenLoop")
+    tabs = pw.Tab((0.0, 0.0), (1.0, 1.0), [mainTab, posTab, velTab, torTab, opeTab])
     controlManager["tabs"] = tabs
 
     setpoints = {"Position": 0.0, "Velocity": 0.0}
@@ -136,10 +143,11 @@ def main():
                 controlManager["PositiontextBoxesData"].setTexts(texts)
                 controlManager["VelocitytextBoxesData"].setTexts(texts)
                 controlManager["TorquetextBoxesData"].setTexts(texts)
+                controlManager["OpenLooptextBoxesData"].setTexts(texts)
                 controlManager["MaintextBoxesData"].setTexts(texts)
 
                 timeNow = round(perf_counter() - baseTime, 2)
-                for name in ["Main", "Position", "Velocity", "Torque"]:
+                for name in ["Main", "Position", "Velocity", "Torque", "OpenLoop"]:
                     numPoints = 1000
                     controlManager[f"{name}plotPos"].addValue(timeNow, data.position, maxLength=numPoints)
                     controlManager[f"{name}plotVel"].addValue(timeNow, data.velocity, maxLength=numPoints)
@@ -149,8 +157,8 @@ def main():
                     controlManager[f"{name}plotVel"].addValue(timeNow, setpoints["Velocity"], 1, maxLength=numPoints)
 
 
-        togNames = ["togglePos", "toggleVel", "toggleTor"]
-        commandValues = [DrivingMode.Position, DrivingMode.Velocity, DrivingMode.Torque]
+        togNames = ["togglePos", "toggleVel", "toggleTor", "toggleOpe"]
+        commandValues = [DrivingMode.Position, DrivingMode.Velocity, DrivingMode.Torque, DrivingMode.OpenLoop]
         for name, valueDefault in zip(togNames, commandValues):
             state = controlManager[name].getValue()
             if (getIfDelta(name, state)):
@@ -171,17 +179,28 @@ def main():
                     print(f"Sending {v} to {baseCommand.value + ix}")
                     if enableSerial: serialComm.send_data(cmd)
 
+        values = controlManager["OpenLoopTuningSliders"].getValue()
+        if getIfDelta("openLoopSpeed", values[0]):
+            print(f"Sending {values[0]} to {CommandType.OpenLoopSpeed.value}")
+            cmd = Command(CommandType.OpenLoopSpeed, 0, values[0])
+            if enableSerial: serialComm.send_data(cmd)
+
+        if getIfDelta("openLoopStrength", values[1]):
+            print(f"Sending {values[1]} to {CommandType.OpenLoopStrength.value}")
+            cmd = Command(CommandType.OpenLoopStrength, 0, values[1])
+            if enableSerial: serialComm.send_data(cmd)
 
         renderTime = controlManager.getRenderTime()
         # print(f"Rendertime: {renderTime}")
         controlManager.update()
 
-    cmd = Command(CommandType.DrivingModeCommand, DrivingMode.Disabled)
-    serialComm.send_data(cmd)
+    if enableSerial:
+        cmd = Command(CommandType.DrivingModeCommand, DrivingMode.Disabled)
+        serialComm.send_data(cmd)
 
-    if (serialComm.num_remaining_commands() > 0): print("Disabling motor.")
-    while (serialComm.num_remaining_commands() > 0):
-        sleep(0.1)
+        if (serialComm.num_remaining_commands() > 0): print("Disabling motor.")
+        while (serialComm.num_remaining_commands() > 0):
+            sleep(0.1)
     print("Motor successfully disabled")
 
 if __name__ == "__main__": main()
